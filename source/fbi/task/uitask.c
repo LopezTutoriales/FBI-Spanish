21cca305451b4a6c210cc0f523a644b838ae50de
López Tutoriales <5817696+LopezTutoriales@users.noreply.github.com>
2021-04-01 21:47:28 +0200

    Add files via upload

--- source/fbi/task/uitask.c
+++ source/fbi/task/uitask.c
@@ -67,10 +67,10 @@ void task_draw_ext_save_data_info(ui_view* view, void* data, float x1, float y1,
     char infoText[512];
 
     snprintf(infoText, sizeof(infoText),
-             "Ext Save Data ID: %016llX\n"
-                     "Shared: %s",
+             "ID de datos externos guardados: %016llX\n"
+                     "Compartido: %s",
              info->extSaveDataId,
-             info->shared ? "Yes" : "No");
+             info->shared ? "Si" : "No");
 
     float infoWidth;
     screen_get_string_size(&infoWidth, NULL, infoText, 0.5f, 0.5f);
@@ -87,18 +87,18 @@ void task_draw_file_info(ui_view* view, void* data, float x1, float y1, float x2
     size_t infoTextPos = 0;
 
     if(strlen(info->name) > 48) {
-        infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Name: %.45s...\n", info->name);
+        infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Nombre: %.45s...\n", info->name);
     } else {
-        infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Name: %.48s\n", info->name);
+        infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Nombre: %.48s\n", info->name);
     }
 
-    infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Attributes: ");
+    infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Atributos: ");
 
     if(info->attributes & (FS_ATTRIBUTE_DIRECTORY | FS_ATTRIBUTE_HIDDEN | FS_ATTRIBUTE_ARCHIVE | FS_ATTRIBUTE_READ_ONLY)) {
         bool needsSeparator = false;
 
         if(info->attributes & FS_ATTRIBUTE_DIRECTORY) {
-            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Directory");
+            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Directorio");
             needsSeparator = true;
         }
 
@@ -107,7 +107,7 @@ void task_draw_file_info(ui_view* view, void* data, float x1, float y1, float x2
                 infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, ", ");
             }
 
-            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Hidden");
+            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Oculto");
             needsSeparator = true;
         }
 
@@ -116,7 +116,7 @@ void task_draw_file_info(ui_view* view, void* data, float x1, float y1, float x2
                 infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, ", ");
             }
 
-            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Archive");
+            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Archivo");
             needsSeparator = true;
         }
 
@@ -125,17 +125,17 @@ void task_draw_file_info(ui_view* view, void* data, float x1, float y1, float x2
                 infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, ", ");
             }
 
-            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Read Only");
+            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Solo lectura");
             needsSeparator = true;
         }
     } else {
-        infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "None");
+        infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Ninguno");
     }
 
     infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "\n");
 
     if(!(info->attributes & FS_ATTRIBUTE_DIRECTORY)) {
-        infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Size: %.2f %s\n",
+        infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Tamaño: %.2f %s\n",
                                 ui_get_display_size(info->size), ui_get_display_size_units(info->size));
 
         if(info->isCia && info->ciaInfo.loaded) {
@@ -146,21 +146,21 @@ void task_draw_file_info(ui_view* view, void* data, float x1, float y1, float x2
 
                 smdh_region_to_string(regionString, info->ciaInfo.meta.region, sizeof(regionString));
             } else {
-                snprintf(regionString, sizeof(regionString), "Unknown");
+                snprintf(regionString, sizeof(regionString), "Desconocida");
             }
 
             infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos,
-                                    "Title ID: %016llX\n"
+                                    "ID del titulo: %016llX\n"
                                             "Version: %hu (%d.%d.%d)\n"
                                             "Region: %s\n"
-                                            "Installed Size: %.2f %s",
+                                            "Tamaño instalado: %.2f %s",
                                     info->ciaInfo.titleId,
                                     info->ciaInfo.version, (info->ciaInfo.version >> 10) & 0x3F, (info->ciaInfo.version >> 4) & 0x3F, info->ciaInfo.version & 0xF,
                                     regionString,
                                     ui_get_display_size(info->ciaInfo.installedSize),
                                     ui_get_display_size_units(info->ciaInfo.installedSize));
         } else if(info->isTicket && info->ticketInfo.loaded) {
-            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "Ticket ID: %016llX", info->ticketInfo.titleId);
+            infoTextPos += snprintf(infoText + infoTextPos, sizeof(infoText) - infoTextPos, "ID de ticket: %016llX", info->ticketInfo.titleId);
         }
     }
 
@@ -178,11 +178,11 @@ void task_draw_pending_title_info(ui_view* view, void* data, float x1, float y1,
     char infoText[512];
 
     snprintf(infoText, sizeof(infoText),
-             "Pending Title ID: %016llX\n"
-                     "Media Type: %s\n"
+             "ID de titulo pendiente: %016llX\n"
+                     "Tipo de medio: %s\n"
                      "Version: %hu (%d.%d.%d)",
              info->titleId,
-             info->mediaType == MEDIATYPE_NAND ? "NAND" : info->mediaType == MEDIATYPE_SD ? "SD" : "Game Card",
+             info->mediaType == MEDIATYPE_NAND ? "NAND" : info->mediaType == MEDIATYPE_SD ? "SD" : "Cartucho",
              info->version, (info->version >> 10) & 0x3F, (info->version >> 4) & 0x3F, info->version & 0xF);
 
     float infoWidth;
@@ -198,7 +198,7 @@ void task_draw_system_save_data_info(ui_view* view, void* data, float x1, float 
 
     char infoText[512];
 
-    snprintf(infoText, sizeof(infoText), "System Save Data ID: %08lX", info->systemSaveDataId);
+    snprintf(infoText, sizeof(infoText), "ID de datos guardados de sistema: %08lX", info->systemSaveDataId);
 
     float infoWidth;
     screen_get_string_size(&infoWidth, NULL, infoText, 0.5f, 0.5f);
@@ -214,7 +214,7 @@ void task_draw_ticket_info(ui_view* view, void* data, float x1, float y1, float 
     if(info->loaded) {
         char infoText[512];
 
-        snprintf(infoText, sizeof(infoText), "Title ID: %016llX", info->titleId);
+        snprintf(infoText, sizeof(infoText), "ID de titulo: %016llX", info->titleId);
 
         float infoWidth;
         screen_get_string_size(&infoWidth, NULL, infoText, 0.5f, 0.5f);
@@ -235,20 +235,20 @@ void task_draw_title_info(ui_view* view, void* data, float x1, float y1, float x
 
         smdh_region_to_string(regionString, info->meta.region, sizeof(regionString));
     } else {
-        snprintf(regionString, sizeof(regionString), "Unknown");
+        snprintf(regionString, sizeof(regionString), "Desconocida");
     }
 
     char infoText[512];
 
     snprintf(infoText, sizeof(infoText),
-             "Title ID: %016llX\n"
-                     "Media Type: %s\n"
+             "ID de titulo: %016llX\n"
+                     "Tipo de medio: %s\n"
                      "Version: %hu (%d.%d.%d)\n"
-                     "Product Code: %s\n"
+                     "Codigo de producto: %s\n"
                      "Region: %s\n"
-                     "Size: %.2f %s",
+                     "Tamaño: %.2f %s",
              info->titleId,
-             info->mediaType == MEDIATYPE_NAND ? "NAND" : info->mediaType == MEDIATYPE_SD ? "SD" : "Game Card",
+             info->mediaType == MEDIATYPE_NAND ? "NAND" : info->mediaType == MEDIATYPE_SD ? "SD" : "Cartucho",
              info->version, (info->version >> 10) & 0x3F, (info->version >> 4) & 0x3F, info->version & 0xF,
              info->productCode,
              regionString,
